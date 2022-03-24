8620e7862a58a626e4fec4b94821e5f394a31b0e
vishnuoum <vishnuoumvishnuoum123@gmail.com>
2022-03-24 21:07:55 +0530

    Added message copying feature

--- lib/coder/pages/chatList.dart
+++ lib/coder/pages/chatList.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:intl/intl.dart';
 import 'package:mingle_box/coder/services/chat.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -125,168 +126,179 @@ class _CoderChatListState extends State<CoderChatList> {
     loadSocket=true;
     print("hello");
     showModalBottomSheet(enableDrag: true,isScrollControlled: true,shape: RoundedRectangleBorder(borderRadius: BorderRadius.only(topRight: Radius.circular(10),topLeft: Radius.circular(10))),context: context, builder: (BuildContext context){
-      return WillPopScope(
-        onWillPop: ()async{
-          loading=true;
-          loadChatList();
-          return true;
-        },
-        child: StatefulBuilder(builder: (BuildContext context,setState)
-        {
-          if(loadSocket) {
-            socket.on('newMessage', (data) {
-              if (senderId == data["sender"]) {
-                try {
-                  setState(() {
-                    messages.add(data);
-                  });
-                }catch(e){}
-              }
-              print(messages);
-            });
-            loadSocket=false;
-          }
-          return Container(
-            decoration: BoxDecoration(
-                color: Colors.blue[200]
-            ),
-            padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
-            child: GestureDetector(
-              onDoubleTap: (){
-                FocusScope.of(context).unfocus();
-              },
-              child: SizedBox.expand(
-                child: Column(
-                  children: [
-                    Container(
-                      height: 90,
-                      width: double.infinity,
-                      decoration: BoxDecoration(
-                          color: Colors.blue
-                      ),
-                      padding: EdgeInsets.only(top: 30),
-                      child: Row(
-                        children: [
-                          IconButton(onPressed: (){
-                            loading=true;
-                            loadChatList();
-                            Navigator.pop(context);
-                          }, icon: Icon(Icons.arrow_back),
-                            color: Colors.white,),
-                          SizedBox(width: 20,),
-                          Text(senderName,style: TextStyle(fontSize: 20,color: Colors.white),)
-                        ],
-                      ),
-                    ),
-                    Expanded(
-                        flex: 19,
-                        child: ListView.builder(
-                          reverse: true,
-                          controller: scrollController,
-                          padding: EdgeInsets.only(left: 20,right: 20,bottom: 10),
-                          itemCount: messages.length,
-                          itemBuilder: (context,index){
-                            index=messages.length - 1 - index;
-                            return Align(
-                              alignment: messages[index]["sender"]!="You"?Alignment.centerLeft:Alignment.centerRight,
-                              child: Container(
-                                padding: EdgeInsets.all(10),
-                                decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(15),
-                                    color: messages[index]["sender"]!="You"?Colors.blue[50]:Colors.white
-                                ),
-                                constraints: BoxConstraints(
-                                    maxWidth: MediaQuery.of(context).size.width*0.65
-                                ),
-                                margin: EdgeInsets.only(top: index!=0?messages[index-1]["sender"]==messages[index]["sender"]?5:12:12,),
-                                child: Column(
-                                  crossAxisAlignment: messages[index]["sender"]!="You"?CrossAxisAlignment.start:CrossAxisAlignment.end,
-                                  children: [
-                                    // index!=0?messages[index-1]["sender"]==messages[index]["sender"]?SizedBox():Padding(
-                                    //   padding: const EdgeInsets.only(bottom: 5.0),
-                                    //   child: Text(messages[index]["sender"],style: TextStyle(fontSize: 11),),
-                                    // ):Padding(
-                                    //   padding: const EdgeInsets.only(bottom: 5.0),
-                                    //   child: Text(messages[index]["sender"],style: TextStyle(fontSize: 11),),
-                                    // ),
-                                    Padding(padding: messages[index]["sender"]=="You"?EdgeInsets.only(right: 5):EdgeInsets.only(left: 5),child: Text(messages[index]["message"],style: TextStyle(fontSize: 16),),),
-                                    Padding(padding: EdgeInsets.only(top: 5),child: Text( DateFormat('dd/MM/yyyy hh:mm a').format(DateTime.parse(messages[index]["dateTime"])),style: TextStyle(fontSize: 11),),)
-                                  ],
-                                ),
-                              ),
-                            );
-                          },
-                        )
-                    ),
-                    Container(
-                      constraints: BoxConstraints(
-                          minHeight: 50,
-                          maxHeight: 80
-                      ),
-                      decoration: BoxDecoration(
-                          color: Colors.blue[200]
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
+      return Scaffold(
+        body: WillPopScope(
+          onWillPop: ()async{
+            loading=true;
+            loadChatList();
+            return true;
+          },
+          child: StatefulBuilder(builder: (BuildContext context,setState)
+          {
+            if(loadSocket) {
+              socket.on('newMessage', (data) {
+                if (senderId == data["sender"]) {
+                  try {
+                    setState(() {
+                      messages.add(data);
+                    });
+                  }catch(e){}
+                }
+                print(messages);
+              });
+              loadSocket=false;
+            }
+            return Container(
+              decoration: BoxDecoration(
+                  color: Colors.blue[200]
+              ),
+              padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+              child: GestureDetector(
+                onDoubleTap: (){
+                  FocusScope.of(context).unfocus();
+                },
+                child: SizedBox.expand(
+                  child: Column(
+                    children: [
+                      Container(
+                        height: 90,
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                            color: Colors.blue
+                        ),
+                        padding: EdgeInsets.only(top: 30),
                         child: Row(
                           children: [
-                            Expanded(
-                              flex:5,
-                              child: Padding(
-                                padding: const EdgeInsets.only(left:5.0,right: 5),
-                                child: Container(
-                                  constraints: BoxConstraints(
-                                      minHeight: 30,
-                                      maxHeight: 80
-                                  ),
-                                  decoration: BoxDecoration(
-                                      color: Colors.white,
-                                      borderRadius: BorderRadius.circular(30)
-                                  ),
-                                  child: TextField(
-                                    controller: msg,
-                                    keyboardType: TextInputType.multiline,
-                                    maxLines: null,
-                                    decoration: InputDecoration(
-                                        hintText: "Type Your Message",
-                                        border: InputBorder.none,
-                                        contentPadding: EdgeInsets.symmetric(horizontal: 20,vertical: 10)
-                                    ),
-                                    onChanged: (text){
-                                      setState(() {});
-                                    },
-                                  ),
-                                ),
-                              ),
-                            ),
-                            msg.text.length==0?SizedBox():Expanded(
-                                child: TextButton(
-                                  child: Icon(Icons.send),
-                                  onPressed: (){
-                                    print("Send");
-                                    setState(() {
-                                      print(msg.text);
-                                      messages.add({"sender":"You","message":msg.text,"dateTime":"${DateTime.now()}"});
-                                      // Future.delayed(Duration(milliseconds: 20),(){
-                                      //   scrollController.position.jumpTo(scrollController.position.maxScrollExtent);
-                                      // });
-                                    });
-                                    socket.emit('sendMessage', {"sender":sharedPreferences.getString("mail"),"senderType":"coder","message":msg.text,"receiver":sender,"receiverType":"buyer"});
-                                    msg.clear();
-                                  },
-                                  style: TextButton.styleFrom(backgroundColor: Colors.blue,shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(100)),padding: EdgeInsets.all(12),primary: Colors.white),
-                                )
-                            )
+                            IconButton(onPressed: (){
+                              loading=true;
+                              loadChatList();
+                              Navigator.pop(context);
+                            }, icon: Icon(Icons.arrow_back),
+                              color: Colors.white,),
+                            SizedBox(width: 20,),
+                            Text(senderName,style: TextStyle(fontSize: 20,color: Colors.white),)
                           ],
                         ),
                       ),
-                    )
-                  ],
+                      Expanded(
+                          flex: 19,
+                          child: ListView.builder(
+                            reverse: true,
+                            controller: scrollController,
+                            padding: EdgeInsets.only(left: 20,right: 20,bottom: 10),
+                            itemCount: messages.length,
+                            itemBuilder: (context,index){
+                              index=messages.length - 1 - index;
+                              return Align(
+                                alignment: messages[index]["sender"]!="You"?Alignment.centerLeft:Alignment.centerRight,
+                                child: GestureDetector(
+                                  onLongPress: (){
+                                    print(messages[index]["message"]);
+                                    Clipboard.setData(ClipboardData(text: messages[index]["message"]));
+                                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                                      content: Text("Copied Message"),
+                                    ));
+                                  },
+                                  child: Container(
+                                    padding: EdgeInsets.all(10),
+                                    decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(15),
+                                        color: messages[index]["sender"]!="You"?Colors.blue[50]:Colors.white
+                                    ),
+                                    constraints: BoxConstraints(
+                                        maxWidth: MediaQuery.of(context).size.width*0.65
+                                    ),
+                                    margin: EdgeInsets.only(top: index!=0?messages[index-1]["sender"]==messages[index]["sender"]?5:12:12,),
+                                    child: Column(
+                                      crossAxisAlignment: messages[index]["sender"]!="You"?CrossAxisAlignment.start:CrossAxisAlignment.end,
+                                      children: [
+                                        // index!=0?messages[index-1]["sender"]==messages[index]["sender"]?SizedBox():Padding(
+                                        //   padding: const EdgeInsets.only(bottom: 5.0),
+                                        //   child: Text(messages[index]["sender"],style: TextStyle(fontSize: 11),),
+                                        // ):Padding(
+                                        //   padding: const EdgeInsets.only(bottom: 5.0),
+                                        //   child: Text(messages[index]["sender"],style: TextStyle(fontSize: 11),),
+                                        // ),
+                                        Padding(padding: messages[index]["sender"]=="You"?EdgeInsets.only(right: 5):EdgeInsets.only(left: 5),child: Text(messages[index]["message"],style: TextStyle(fontSize: 16),),),
+                                        Padding(padding: EdgeInsets.only(top: 5),child: Text( DateFormat('dd/MM/yyyy hh:mm a').format(DateTime.parse(messages[index]["dateTime"])),style: TextStyle(fontSize: 11),),)
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              );
+                            },
+                          )
+                      ),
+                      Container(
+                        constraints: BoxConstraints(
+                            minHeight: 50,
+                            maxHeight: 80
+                        ),
+                        decoration: BoxDecoration(
+                            color: Colors.blue[200]
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            children: [
+                              Expanded(
+                                flex:5,
+                                child: Padding(
+                                  padding: const EdgeInsets.only(left:5.0,right: 5),
+                                  child: Container(
+                                    constraints: BoxConstraints(
+                                        minHeight: 30,
+                                        maxHeight: 80
+                                    ),
+                                    decoration: BoxDecoration(
+                                        color: Colors.white,
+                                        borderRadius: BorderRadius.circular(30)
+                                    ),
+                                    child: TextField(
+                                      controller: msg,
+                                      keyboardType: TextInputType.multiline,
+                                      maxLines: null,
+                                      decoration: InputDecoration(
+                                          hintText: "Type Your Message",
+                                          border: InputBorder.none,
+                                          contentPadding: EdgeInsets.symmetric(horizontal: 20,vertical: 10)
+                                      ),
+                                      onChanged: (text){
+                                        setState(() {});
+                                      },
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              msg.text.length==0?SizedBox():Expanded(
+                                  child: TextButton(
+                                    child: Icon(Icons.send),
+                                    onPressed: (){
+                                      print("Send");
+                                      setState(() {
+                                        print(msg.text);
+                                        messages.add({"sender":"You","message":msg.text,"dateTime":"${DateTime.now()}"});
+                                        // Future.delayed(Duration(milliseconds: 20),(){
+                                        //   scrollController.position.jumpTo(scrollController.position.maxScrollExtent);
+                                        // });
+                                      });
+                                      socket.emit('sendMessage', {"sender":sharedPreferences.getString("mail"),"senderType":"coder","message":msg.text,"receiver":sender,"receiverType":"buyer"});
+                                      msg.clear();
+                                    },
+                                    style: TextButton.styleFrom(backgroundColor: Colors.blue,shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(100)),padding: EdgeInsets.all(12),primary: Colors.white),
+                                  )
+                              )
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               ),
-            ),
-          );
-        }),
+            );
+          }),
+        ),
       );
     });
   }
